01a79f7c09cb88e2666b37c6b2f63a59c31c044b
OldRoge <rogeriomrjr@icloud.com>
2021-03-31 20:05:23 -0300

    Correting sintaxes mistakes

--- Fundamentos do Desenvolvimento Web/exercises/introducao-a-javascript-e-logica-de-programacao_1/script1.js
+++ Fundamentos do Desenvolvimento Web/exercises/introducao-a-javascript-e-logica-de-programacao_1/script1.js
@@ -1,34 +1,34 @@
 // 1 - Faça 5 programas, um para cada operação aritmética básica. Seu programa deve ter duas variáveis, a e b , definidas no começo com os valores que serão operados. Faça programas para:
 
 // - Adição
-var a = 2
-var b = 5
-sum = a + b
+var a = 2;
+var b = 5;
+sum = a + b;
 
 console.log(sum)
 // - Subtração
-var a = 2
-var b = 5
-subtract = a - b
+var a = 2;
+var b = 5;
+subtract = a - b;
 
 console.log(subtract)
 // - Multiplicação
-var a = 2
-var b = 5
-multiply = a * b
+var a = 2;
+var b = 5;
+multiply = a * b;
 
 console.log(multiply)
 
 // - Divisão
-var a = 2
-var b = 5
-divide = a / b
+var a = 2;
+var b = 5;
+divide = a / b;
 
 console.log(divide)
 
 // - Módulo
-var a = 5
-var b = 2
-modulus = a % b
+var a = 5;
+var b = 2;
+modulus = a % b;
 
 console.log(modulus)
