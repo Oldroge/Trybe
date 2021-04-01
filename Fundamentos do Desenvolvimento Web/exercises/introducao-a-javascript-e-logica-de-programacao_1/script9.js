acb20ff14c2761099696ba1d08757ef131b7fc64
OldRoge <rogeriomrjr@icloud.com>
2021-04-01 00:18:03 -0300

    Write a program that define three numbers in variables in the beginning a return 'True' at least one of three is odd. Otherwise, it returns 'False'.

---
+++ Fundamentos do Desenvolvimento Web/exercises/introducao-a-javascript-e-logica-de-programacao_1/script9.js
@@ -0,0 +1,14 @@
+/*9 - Escreva um programa que defina três números em variáveis no seu começo e retorne true se pelo menos uma das três for ímpar. Caso contrário, ele retorna false .
+Bônus: Use somente if */
+
+var a = 1
+var b = 3
+var c = 2
+
+if(a % 2 > 0 || b % 2 > 0 || c % 2 > 0) {
+  console.log('True')
+}
+
+else {
+  console.log('False')
+}
