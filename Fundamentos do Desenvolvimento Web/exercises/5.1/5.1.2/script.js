49215659b5dda0e3bfbcb68f2c765313b1721faa
OldRoge <rogeriomrjr@icloud.com>
2021-04-06 23:21:57 -0300

    Set a background color to the first subtitles

--- Fundamentos do Desenvolvimento Web/exercises/5.1/5.1.2/script.js
+++ Fundamentos do Desenvolvimento Web/exercises/5.1/5.1.2/script.js
@@ -8,3 +8,10 @@ let emergencyIndex;
 for (emergencyIndex = 0; emergencyIndex < emergency.length; emergencyIndex += 1) {
   emergency[0].style.backgroundColor = 'lightSalmon';
 }
+
+// Background color for subtitle h3
+let subtitlePurple = document.querySelectorAll('h3#subtitlePurple');
+let h3Purple;
+for (h3Purple = 0; h3Purple < subtitlePurple.length; h3Purple += 1) {
+  subtitlePurple[h3Purple].style.backgroundColor = 'mediumorchid';
+}
