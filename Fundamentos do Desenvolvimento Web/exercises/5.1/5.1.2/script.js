fc4b1ef72408488cc2d0fcbf82d0c7155f57bf91
OldRoge <rogeriomrjr@icloud.com>
2021-04-06 23:20:40 -0300

    Adding background color for the first block

--- Fundamentos do Desenvolvimento Web/exercises/5.1/5.1.2/script.js
+++ Fundamentos do Desenvolvimento Web/exercises/5.1/5.1.2/script.js
@@ -1,3 +1,10 @@
 // Background-color for Title
 let head =  document.getElementById('header-container');
 head.style.backgroundColor = ('MediumSeaGreen');
+
+// Background color for Div
+let emergency = document.getElementsByClassName('emergency-tasks');
+let emergencyIndex;
+for (emergencyIndex = 0; emergencyIndex < emergency.length; emergencyIndex += 1) {
+  emergency[0].style.backgroundColor = 'lightSalmon';
+}
