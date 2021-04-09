ad537fd41a985daab8972ae412b2da9e09a91701
OldRoge <rogeriomrjr@icloud.com>
2021-04-09 03:07:47 -0300

    exercises II - 2. Create a son to 'elementoOndeVoceEsta'

--- Fundamentos do Desenvolvimento Web/exercises/5.2/5.2.1/script.js
+++ Fundamentos do Desenvolvimento Web/exercises/5.2/5.2.1/script.js
@@ -1,3 +1,7 @@
 let whereIsMySon = document.querySelector('#pai');
 let hiDad = document.createElement('div');
 whereIsMySon.appendChild(hiDad);
+
+let birth = document.querySelector('#elementoOndeVoceEsta');
+let baby = document.createElement('div');
+birth.appendChild(baby);
