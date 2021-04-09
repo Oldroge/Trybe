d3b681a5100e175034440352d8a819da16fe0d0c
OldRoge <rogeriomrjr@icloud.com>
2021-04-09 03:10:24 -0300

    exercises II - 3. Create a son to ''primeiroFilhoDoFilho

--- Fundamentos do Desenvolvimento Web/exercises/5.2/5.2.1/script.js
+++ Fundamentos do Desenvolvimento Web/exercises/5.2/5.2.1/script.js
@@ -5,3 +5,7 @@ whereIsMySon.appendChild(hiDad);
 let birth = document.querySelector('#elementoOndeVoceEsta');
 let baby = document.createElement('div');
 birth.appendChild(baby);
+
+let firstSon = document.querySelector('#primeiroFilhoDoFilho');
+let sonOfSon = document.createElement('div');
+firstSon.appendChild(sonOfSon);
