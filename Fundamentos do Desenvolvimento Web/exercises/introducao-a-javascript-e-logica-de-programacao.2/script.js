84f3586cc5f6d633585201715fa447ca3817bc81
OldRoge <rogeriomrjr@icloud.com>
2021-04-21 17:39:21 -0300

    For the third exercise, calculate and print the arithmetic average of values contained in array

--- Fundamentos do Desenvolvimento Web/exercises/introducao-a-javascript-e-logica-de-programacao.2/script.js
+++ Fundamentos do Desenvolvimento Web/exercises/introducao-a-javascript-e-logica-de-programacao.2/script.js
@@ -2,14 +2,25 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 /**1. Nesse primeiro exercício, percorra o array imprimido todos os valores nele contidos com a função console.log() */
 
-for (let cont in numbers) {
-  console.log(numbers[cont]);
-}
+// for (let count in numbers) {
+//   console.log(numbers[count]);
+// }
 
 /**2. Para o segundo exercício, você deve somar todos os valores contidos no array e imprimir o resultado */
 
-let = total = 0;
-for (let sum in numbers) {
-  total += numbers[sum];
+//  let = total = 0;
+//  for (let sum in numbers) {
+//    total += numbers[sum];
+//  }
+//  console.log(total)
+
+/**3. Para o terceiro exercício, calcule e imprima a média aritmética dos valores contidos no array */
+
+let counter = 0;
+let total = 0;
+for (let average in numbers) {
+  total += numbers[average];
+  counter = average;  
 }
-console.log(total)
+console.log(total / counter);
+
