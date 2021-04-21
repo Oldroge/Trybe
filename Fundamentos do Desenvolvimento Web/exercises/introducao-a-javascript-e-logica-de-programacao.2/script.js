a2df353d99bad1e0ca886743d4cc94a338d758d8
OldRoge <rogeriomrjr@icloud.com>
2021-04-21 17:23:10 -0300

    For the second exercise, you should sum all values contained in array and print the result

--- Fundamentos do Desenvolvimento Web/exercises/introducao-a-javascript-e-logica-de-programacao.2/script.js
+++ Fundamentos do Desenvolvimento Web/exercises/introducao-a-javascript-e-logica-de-programacao.2/script.js
@@ -5,3 +5,11 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 for (let cont in numbers) {
   console.log(numbers[cont]);
 }
+
+/**2. Para o segundo exercício, você deve somar todos os valores contidos no array e imprimir o resultado */
+
+let = total = 0;
+for (let sum in numbers) {
+  total += numbers[sum];
+}
+console.log(total)
