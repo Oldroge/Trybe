a6aab39c54312d280843e440098422b967d1b521
OldRoge <rogeriomrjr@icloud.com>
2021-04-21 18:26:30 -0300

    Create the exercise 5

--- Fundamentos do Desenvolvimento Web/exercises/introducao-a-javascript-e-logica-de-programacao.2/script.js
+++ Fundamentos do Desenvolvimento Web/exercises/introducao-a-javascript-e-logica-de-programacao.2/script.js
@@ -26,16 +26,19 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 /**4. Com o mesmo código do exercício anterior, caso valor final seja maior que 20, imprima a mensagem "valor maior que 20". Caso não seja, imprima a mensagem: "valor menor ou igual a 20" */
 
-let counter =  0;
-let total = 0;
-for (let average in numbers) {
-  total += numbers[average];
-  counter = average;
-}
-let divide = total / counter;
-console.log(divide);
-if (divide > 20) {
-  console.log('Valor maior que 20')
-} else {
-  console.log('Valor menor ou ingual a 20')
-}
+// let counter =  0;
+// let total = 0;
+// for (let average in numbers) {
+//   total += numbers[average];
+//   counter = average;
+// }
+// let divide = total / counter;
+// console.log(divide);
+// if (divide > 20) {
+//   console.log('Valor maior que 20')
+// } else {
+//   console.log('Valor menor ou ingual a 20')
+// }
+
+/**5. Utilizando for, descubra qual o maior valor contido no array e imprima-o */
+
