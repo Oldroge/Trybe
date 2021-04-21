e153186e6fe43346c1271610d63b199866ec90cb
OldRoge <rogeriomrjr@icloud.com>
2021-04-21 17:14:35 -0300

    In this first exercise, go through the array printing all values contained with the function console.log()

--- Fundamentos do Desenvolvimento Web/exercises/introducao-a-javascript-e-logica-de-programacao.2/script.js
+++ Fundamentos do Desenvolvimento Web/exercises/introducao-a-javascript-e-logica-de-programacao.2/script.js
@@ -1,1 +1,7 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+/**1. Nesse primeiro exercício, percorra o array imprimido todos os valores nele contidos com a função console.log() */
+
+for (let cont in numbers) {
+  console.log(numbers[cont]);
+}
