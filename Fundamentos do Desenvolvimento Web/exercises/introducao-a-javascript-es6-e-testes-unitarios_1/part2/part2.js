d841b613739f9082593404811d6875c6c3c40215
OldRoge <rogeriomrjr@icloud.com>
2021-04-27 10:08:22 -0300

    Done the part 2 exercise 1

---
+++ Fundamentos do Desenvolvimento Web/exercises/introducao-a-javascript-es6-e-testes-unitarios_1/part2/part2.js
@@ -0,0 +1,12 @@
+/* 1. Crie uma função que receba um número e retorne seu fatorial.
+
+- Na matemática, o fatorial de um número não negativo N , com a notação N! , é o produto de todos os inteiros menores ou iguais a N . Exemplo: 4! = 4 * 3 * 2 * 1 = 24.
+
+- Bônus (opcional): tente fazer o mesmo exercício de forma recursiva . Spoiler: É possível resolver com uma linha usando ternary operator .
+*/
+
+function factorial(n) {
+  let result = n === 0 ? 1 : n * factorial(n-1);
+  return result;
+}
+console.log(factorial(7));
