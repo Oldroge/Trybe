c0ba2ddf873d79b596e79f014916e20a19b54448
OldRoge <rogeriomrjr@icloud.com>
2021-04-26 21:59:32 -0300

    Done the second exercises from part 1

--- Fundamentos do Desenvolvimento Web/exercises/introducao-a-javascript-es6-e-testes-unitarios_1/part1/exercise1.js
+++ Fundamentos do Desenvolvimento Web/exercises/introducao-a-javascript-es6-e-testes-unitarios_1/part1/exercise1.js
@@ -16,7 +16,9 @@ testingScope(true);
 
 1. Faça as modificações necessárias na função para que o seu comportamento respeite o escopo no qual cada variável foi declarada.
 
-- Modifique a estrutura da função para que ela seja uma arrow function. */
+- Modifique a estrutura da função para que ela seja uma arrow function. 
+
+- Modifique as concatenações para template literals . */   
 const testingScopeFunction = escopo => {
   const ifScopo = (escopo === true) ? `Ótimo, fui utilizada no escopo!` : `Não devo ser utilizada fora do meu escopo (if)`;
   console.log(ifScopo)
@@ -24,13 +26,21 @@ const testingScopeFunction = escopo => {
   const elseScopo = (escopo === true) ? `:D` : `O que estou fazendo aqui? :O`
   console.log(elseScopo)
 }
-testingScopeFunction(true);
+testingScopeFunction(true)
 
-/* - Modifique as concatenações para template literals . */      
+   
+/* 2. Copie o código abaixo e faça uma função que retorne o array oddsAndEvens em ordem crescente.
 
+ - Utilize template literals para que a chamada console.log(oddsAndEvens); retorne "Os números 2,3,4,7,10,13 se encontram ordenados de forma crescente!".
 
-      // const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+ - Bônus (opcional): tente fazer o mesmo exercício utilizando o método array.sort() . Spoiler: É possível realizar uma função que ordene qualquer array de números.
 
-      // // Seu código aqui.
+ - Copie o código abaixo. */
 
-      // console.log(oddsAndEvens);
+let oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+oddsAndEvens.sort(function(a, b) {
+  return a - b;
+})
+console.log(oddsAndEvens);
+ 
