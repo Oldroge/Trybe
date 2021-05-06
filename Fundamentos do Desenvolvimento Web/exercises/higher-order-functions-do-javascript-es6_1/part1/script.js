e663bba2d25af99c0bf15e4a256849bade53377f
OldRoge <rogeriomrjr@icloud.com>
2021-05-06 10:43:34 -0300

    Done the exercise 2

--- Fundamentos do Desenvolvimento Web/exercises/higher-order-functions-do-javascript-es6_1/part1/script.js
+++ Fundamentos do Desenvolvimento Web/exercises/higher-order-functions-do-javascript-es6_1/part1/script.js
@@ -16,3 +16,14 @@ console.log(newEmployees());
 
 /* 2. Desenvolva uma HOF que retorna o resultado de um sorteio. Esta HOF irá gerar um número aleatório entre 1 e 5 recebendo como parâmetros o número apostado e uma função que checa se o número apostado é igual ao número sorteado. O retorno da sua HOF deve ser uma string (Ex: "Tente novamente" ou "Parabéns você ganhou").
 */
+
+const randomNumbers = () => Math.ceil(Math.random() * 5);
+console.log(randomNumbers());
+
+function sortedNumber(num, random) {
+  if (num === random) {
+    return 'Parabéns, você ganhou!'
+  }
+  return 'Tente novamente! ):'
+}
+console.log(sortedNumber(4, randomNumbers()));
