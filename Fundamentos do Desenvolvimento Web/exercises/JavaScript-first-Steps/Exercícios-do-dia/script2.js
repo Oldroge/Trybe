6bf5cbfda133655787ff703e70bcd1ed1e1145ca
OldRoge <rogeriomrjr@icloud.com>
2021-05-10 19:23:37 -0300

    Done exercise 2

---
+++ Fundamentos do Desenvolvimento Web/exercises/JavaScript-first-Steps/Exercícios-do-dia/script2.js
@@ -0,0 +1,9 @@
+/*2. Faça um programa que retorne o maior de dois números. Defina no começo do programa duas variáveis com os valores que serão comparados. */
+
+let a = 3;
+let b = 5;
+if (a > b) {
+  console.log('Variable A is bigger = ' + a)
+} else {
+  console.log('Variable B is bigger = ' + b)
+}
