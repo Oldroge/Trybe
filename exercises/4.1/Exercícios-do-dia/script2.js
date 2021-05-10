0516f22d99eec8f5bb80c61c100822b2d8700fd2
OldRoge <rogeriomrjr@icloud.com>
2021-05-10 12:00:48 -0300

    Done exercise 3

---
+++ exercises/4.1/Exercícios-do-dia/script2.js
@@ -0,0 +1,12 @@
+/*3. Faça um programa que retorne o maior de três números. Defina no começo do programa três variáveis com os valores que serão comparados. */
+
+let a = 4;
+let b = 7;
+let c = 3;
+if (a > b && a > c) {
+  console.log('Letra A = ' + a);
+} else if (b > a && b > c) {
+  console.log('Letra B = ' + b)
+} else {
+  console.log('Letra C = ' + c)
+}
