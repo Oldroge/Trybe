19cc540d9deb460910f2693e70365b3b34da801e
OldRoge <rogeriomrjr@icloud.com>
2021-05-10 11:49:01 -0300

    Adding some new files to exercises

---
+++ Fundamentos do Desenvolvimento Web/exercises/JavaScript-first-Steps/Exercícios-do-dia/script.js
@@ -0,0 +1,16 @@
+let a = 2
+let b = 3
+let sum = a + b;
+console.log(sum);
+
+let sub = a - b;
+console.log(sub);
+
+let multiply = a * b;
+console.log(multiply);
+
+let div = a / b;
+console.log(div);
+
+let mod = a % b;
+console.log(mod);
