6b21b6d7bed3505222eb3eede4d167b0845a34ea
OldRoge <rogeriomrjr@icloud.com>
2021-05-10 12:08:17 -0300

    Adding a header

--- exercises/4.1/Exercícios-do-dia/script3.js
+++ exercises/4.1/Exercícios-do-dia/script3.js
@@ -1,3 +1,5 @@
+//4. Faça um programa que, dado um valor definido numa variável, retorne "positive" se esse valor for positivo, "negative" se for negativo e "zero" caso contrário.
+
 let positiveOrNegative = -5;
 if (positiveOrNegative >= 0) {
   console.log('Positive');
