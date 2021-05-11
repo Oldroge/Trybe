942db0fbebd94a6eb6028cb98efb2f052c2ecc44
OldRoge <rogeriomrjr@icloud.com>
2021-05-11 18:54:35 -0300

    Done exercise 2

--- Fundamentos do Desenvolvimento Web/exercises/Bloco_9/callBacks/script2.js
+++ Fundamentos do Desenvolvimento Web/exercises/Bloco_9/callBacks/script2.js
@@ -1,16 +1,18 @@
+/*2. No código abaixo você tem a função getUser modificada, que agora funciona de modo assíncrono e imprime dados de uma pessoa qualquer depois de um certo tempo. Complete a função getUser de forma que ela receba um callback para que possa realizar as operações abaixo sobre a pessoa. */
+
 const userFullName = ({ firstName, lastName }) => `Hello! My name is ${firstName} ${lastName}`;
 const userNationality = ({ firstName, nationality }) => `${firstName} is ${nationality}`;
 
 const delay = (maxMilliseconds = 5000) => Math.floor(Math.random() * maxMilliseconds);
 
-const getUser = () => {
+const getUser = toReturn => {
   setTimeout(() => {
     const user = {
       firstName: "Ivan",
       lastName: "Ivanovich",
       nationality: "Russian",
     };
-    console.log(user);
+    console.log(toReturn(user));
   }, delay());
 };
 
