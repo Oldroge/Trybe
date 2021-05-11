420b4ec58aa5c2a9792a48e03be77f3795dced79
OldRoge <rogeriomrjr@icloud.com>
2021-05-10 21:01:38 -0300

    Done exercise 4

---
+++ Fundamentos do Desenvolvimento Web/exercises/JavaScript-first-Steps/Exercícios-do-dia/script4.js
@@ -0,0 +1,7 @@
+/*4. Faça um programa que, dado um valor definido numa variável, retorne "positive" se esse valor for positivo, "negative" se for negativo e "zero" caso contrário. */
+let positiveOrNegative = -1;
+if (positiveOrNegative >= 0) {
+  console.log('Positive')
+} else {
+  console.log('Negative')
+}
