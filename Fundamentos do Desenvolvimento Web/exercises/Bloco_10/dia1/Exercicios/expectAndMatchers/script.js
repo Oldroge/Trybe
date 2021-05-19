0d91f0a09820f1716094dd8780c51317163cfe36
OldRoge <rogeriomrjr@icloud.com>
2021-05-18 22:43:35 -0300

    Done exercise from expect and matchers

---
+++ Fundamentos do Desenvolvimento Web/exercises/Bloco_10/dia1/Exercicios/expectAndMatchers/script.js
@@ -0,0 +1,26 @@
+const arr = [1, 2 ,3];
+const obj = { a: 1, b: 2, c: 3};
+const n = null;
+const z = 0;
+const multiplyByTwo = (number) => {
+  if (!number) {
+    throw new Error('number é indefinido')
+  }
+  return number * 2;
+};
+multiplyByTwo(4);
+multiplyByTwo();
+multiplyByTwo('a');
+
+const workDays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
+const weekDays = ['Sunday', ...workDays, 'Saturday'];
+
+module.exports = {
+  arr,
+  obj,
+  n,
+  z,
+  multiplyByTwo,
+  workDays,
+  weekDays
+}
