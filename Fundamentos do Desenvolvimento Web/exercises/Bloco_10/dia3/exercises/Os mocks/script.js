1e542cbb74ce343ee55457325bd4d8941360612e
OldRoge <rogeriomrjr@icloud.com>
2021-05-20 20:48:22 -0300

    Add some files

---
+++ Fundamentos do Desenvolvimento Web/exercises/Bloco_10/dia3/exercises/Os mocks/script.js
@@ -0,0 +1,8 @@
+const retornaNumeroAleatorio = () => Math.floor(Math.random() * 100);
+
+const divisivelPorDois = () => (retornaNumeroAleatorio() % 2) === 0;
+
+module.exports = {
+  retornaNumeroAleatorio,
+  divisivelPorDois
+}
