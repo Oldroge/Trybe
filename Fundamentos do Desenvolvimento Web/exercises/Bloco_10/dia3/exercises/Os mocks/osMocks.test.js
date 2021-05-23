ff801df5a3a67c9ff011dfb79a8e830f40a3ee74
OldRoge <rogeriomrjr@icloud.com>
2021-05-23 19:52:16 -0300

    Some changes in the files

--- Fundamentos do Desenvolvimento Web/exercises/Bloco_10/dia3/exercises/Os mocks/osMocks.test.js
+++ Fundamentos do Desenvolvimento Web/exercises/Bloco_10/dia3/exercises/Os mocks/osMocks.test.js
@@ -1,4 +1,4 @@
-const script = require('./script');
+const script = require('./osMocks');
 
 test('quando o número aleatório é par, a função retorna `true`', () => {
   expect(script.divisivelPorDois()).toBe(true); // Como garantimos que o número retornado será par?
