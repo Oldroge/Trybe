831d7d8dc0c82a9f18bfc5e80e18efe3a83d84c0
OldRoge <rogeriomrjr@icloud.com>
2021-05-24 23:29:58 -0300

    Classese react

---
+++ Desenvolvimento Front-end/Exercises/Bloco_11/Dia1/Classes-e-React/react-init/src/Component.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class NewComponent extends Component {
+    render() {
+      return(
+        <div>
+          <h1>Roge!</h1>
+          <p>Olá! Meu nome é Rogério Martins e sou estudante de desenvolvimento de software na Trybe.</p>
+        </div>
+      )
+    }
+  }
+
+export default NewComponent;
