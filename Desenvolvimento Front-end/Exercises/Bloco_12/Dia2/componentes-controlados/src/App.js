c974be24068b87eeb8c064631cce5273c2d00c7d
OldRoge <rogeriomrjr@icloud.com>
2021-05-29 11:07:50 -0300

    Trying to make fixation exercises

---
+++ Desenvolvimento Front-end/Exercises/Bloco_12/Dia2/componentes-controlados/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Forms extends React.Component {
+  render() {
+    return (
+      <div>
+        <form>
+          <select  />
+          <input type="email" placeholder="Login"/>
+          <input type="number" placeholder="password"/>
+          <textarea type="text" />
+        </form>
+      </div>
+    )
+  }
+}
+
+export default Forms;
