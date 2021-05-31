d29570668036b4a10e3097273697830653db7fc3
OldRoge <rogeriomrjr@icloud.com>
2021-05-31 10:03:33 -0300

    Some changes on the exercise files

---
+++ Desenvolvimento Front-end/Exercises/Bloco_11/Dia2/exerciciosDoDia/my-pokedex/src/Pokemon.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Data from './data';
+
+class Pokemon extends React.Component {
+  render() {
+    return(
+    <div>
+      <h1>Pokedex</h1>
+      
+    </div>
+    )
+  }
+}
+
+export default Pokemon;
---
+++ Desenvolvimento Front-end/Exercises/Bloco_11/Dia2/exerciciosDoDia/my-pokedex/src/listPokemons.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Data from './data';
+
+class ListPokemons extends React.Component {
+  render() {
+    return (
+      
+    )
+  }
+}
