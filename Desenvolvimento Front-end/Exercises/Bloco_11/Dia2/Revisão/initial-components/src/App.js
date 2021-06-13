9a51b9cfac98eb83b37dadf828d083482eda0195
OldRoge <rogeriomrjr@icloud.com>
2021-06-13 15:01:00 -0300

    Made some review on content

---
+++ Desenvolvimento Front-end/Exercises/Bloco_11/Dia2/Revisão/initial-components/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Greeting from './Greeting';
+import './App.css';
+
+class App extends Component {
+  render() {
+  return (
+  <Greeting name="Roge" age="26" lastname="Martins" />
+
+  );
+  }
+}
+
+export default App;
