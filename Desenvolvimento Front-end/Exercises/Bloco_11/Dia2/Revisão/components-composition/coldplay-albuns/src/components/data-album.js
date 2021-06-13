63ef1101cb4905867078bcb87ef835b39353287a
OldRoge <rogeriomrjr@icloud.com>
2021-06-13 15:47:44 -0300

    Refactoring fixations exercise Coldplay Album

---
+++ Desenvolvimento Front-end/Exercises/Bloco_11/Dia2/Revisão/components-composition/coldplay-albuns/src/components/data-album.js
@@ -0,0 +1,33 @@
+const Albuns = {
+      // Declaração do objeto contendo informações do album01
+       album01: {
+        image: 'https://upload.wikimedia.org/wikipedia/en/thumb/b/b0/Coldplay_-_Mylo_Xyloto.JPG/220px-Coldplay_-_Mylo_Xyloto.JPG',
+        title: 'Mylo Xyloto',
+        releaseDate: {
+          year: '2011',
+          month: '10',
+          day: '24',
+        },
+        others: {
+          recordCompany: 'Capitol, Parlophone',
+          formats: 'CD, digital'
+        }
+      },
+  
+      // Declaração do objeto contendo informações do album02
+       album02: {
+        image: 'https://upload.wikimedia.org/wikipedia/pt/thumb/5/5d/Coldplay_-_Ghost_Stories.jpg/220px-Coldplay_-_Ghost_Stories.jpg',
+        title: 'Ghost Stories',
+        releaseDate: {
+          year: '2014',
+          month: '05',
+          day: '16',
+        },
+        others: {
+          recordCompany: 'Parlophone',
+          formats: 'CD, digital'
+        }
+      },
+};
+
+export default Albuns;
