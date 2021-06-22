ea0fff95749af82c2ff0341ec3e16bb4c5d3d40a
OldRoge <rogeriomrjr@icloud.com>
2021-06-22 10:29:34 -0300

    RTL introduction

--- Desenvolvimento Front-end/Exercises/Bloco_15/Dia1/Aulas/testes-react/src/App.test.js
+++ Desenvolvimento Front-end/Exercises/Bloco_15/Dia1/Aulas/testes-react/src/App.test.js
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
@@ -19,18 +19,31 @@ test('renders learn react link', () => {
 
 
 /*
+NUM PRIMEIRO MOMENTO******
 Neste test ele só funcionaria se constasse apenas um botão sendo renderizado no arquivo, pois o getByRole só faz uma verificação.
 Fazendo o test com dois botões do arquivo sendo renderizado, o test quebra, pois ele encontra 2 botões e espera apenas 1.
 */
-// test('Verificar se existe um botão', () => 
-//   // Buscar elemento
-//   const { getByRole } = render(<App />);
-//   const button = getByRole(/button/i);
 
-//   // Renderiza
-//   expect(button).toBeInTheDocument();
-//   expect(button.type).toBe('button');
-// })
+/*
+NUM SEGUNDO MOMENTO ******
+Foi visto com o código anterior (comentado) não conseguimos realizar o test com dois botões ou o test acaba quebrando. Colocando a propriedade data-testid nos botões conseguimos referenciar um dos botões no teste.
+*/
+test('Verificar se existe um botão de enviar', () => {
+  // Buscar elemento
+  // const { getByRole } = render(<App />);
+  const { getByTestId } = render(<App />);
+  // const button = getByRole(/button/i);
+  const btnSend = getByTestId('id-send');
+
+
+  // Renderiza
+  expect(btnSend).toBeInTheDocument();
+  expect(btnSend.type).toBe('button');
+  expect(btnSend).toHaveValue('Enviar');
+
+  // expect(button).toBeInTheDocument();
+  // expect(button.type).toBe('button');
+})
 
 /**Devido a isso precisão utilizamos outro seletor, getAllByRole */
 test('Verificando se existe dois botões', () => {
@@ -44,5 +57,47 @@ test('Verificando se existe dois botões', () => {
   expect(buttons.length).toBe(2);
 
   /* */
+})
+
+/*
+Simulando test para comportamentos
+*/
+test('Verificando se o botão e o campo email estão funcionando', () => {
+  // Buscar elementos
+  const { getByTestId, getByLabelText } = render(<App />)
+
+  // Criar variável de suposto comportamento do usuário
+  const EMAIL_USER = 'email@email.com.br';
+
+  // Criar variável para a propriedade Test-id
+  const textEmail = getByTestId('id-email-user');
+
+  // Teste espera que ela apareça no documento
+  expect(textEmail).toBeInTheDocument();
+
+  // Teste espera que a propriedade tenha um conteúdo de texto Valor:
+  expect(textEmail).toHaveTextContent('Valor:');
+
+  // Buscar botão, através de variável, pela propriedade test-id
+  const btnSend = getByTestId('id-send');
+
+  // Buscar input, através de variável, pela label que contenha um texto de valor Email
+  const inputEmail = getByLabelText('Email');
+
+  /*
+  FireEvent espera um comportamento de evento, por exemplo: Um click, uma caracter, algo que mude o estado do documento.
+  */
+
+  // Aqui o fireEvent simula a variável de EMAIl_USER criado no início do test para ver o estado altera digitando o suposto email
+  fireEvent.change(inputEmail, { target: { value: EMAIL_USER } });
+
+  // FireEvent clica no botão
+  fireEvent.click(btnSend);
+
+  // fireEvent verificar se clicando no Botão o valur do inputEmail fica em branco
+  expect(inputEmail).toHaveValue('');
+
+  // fireEvent verifica se após clicar no botão o conteúdo de textEmail apareça com o valor de EMAIL_USER
+  expect(textEmail).toHaveTextContent(`Valor: ${EMAIL_USER}`);
 })
 
