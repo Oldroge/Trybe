b4e9a2654bdc5c41c7934bb89695ff97438d8ff7
OldRoge <rogeriomrjr@icloud.com>
2021-08-18 09:43:56 -0300

    Done exercise 3

---
+++ Desenvolvimento Back-end/Bloco_20/dia1/exercíciosDoDia/Exercise3.sql
@@ -0,0 +1,10 @@
+-- Exercise 3: Create a table with the follow constraints:
+-- Table name: Filme
+-- Columns:
+-- filmeId: primary key: int type, increment by 1 each time that a value is insert automatically
+-- Descrição: Disallow null, text type: varchar(100)
+-- AnoLancamento: Disallow null, int type
+-- Nota: Allow null, int type    
+
+ SELECT * FROM roge_teste.Filme;
+ 
