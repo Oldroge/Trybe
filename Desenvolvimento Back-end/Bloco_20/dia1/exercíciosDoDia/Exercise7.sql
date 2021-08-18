352c3993fed54376a2477d1ea7038316de898c99
OldRoge <rogeriomrjr@icloud.com>
2021-08-18 10:24:46 -0300

    Exercise 7

---
+++ Desenvolvimento Back-end/Bloco_20/dia1/exercíciosDoDia/Exercise7.sql
@@ -0,0 +1,3 @@
+-- Exercise 7: Open each table in the database sakila and find at least 3 examples by relationships 1:N or N:1
+
+-- Answer: film and category, country and city, customer and address 
