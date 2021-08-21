bda72e737dc4ffbb93baacde953d60953447fae1
OldRoge <rogeriomrjr@icloud.com>
2021-08-21 16:59:22 -0300

    Exercise 3: Write a query to display the numbers sum 10 and 15;

--- Desenvolvimento Back-end/Bloco_20/dia2/exercisesOfTheDay/practice/exerciseList.sql
+++ Desenvolvimento Back-end/Bloco_20/dia2/exercisesOfTheDay/practice/exerciseList.sql
@@ -1,5 +1,9 @@
 -- Exercise 1: Write a query to display string "This is SQL Exercise, Practice and Solution";
 SELECT 'This is SQL Exercise, Practice and Solution';
 
--- Exercise 2: Write a query to show three numbers and three columns;
+-- Exercise 2: Write a query to display three numbers and three columns;
 SELECT 1 AS First_number, 2 AS second_number, 3 AS third_number;
+
+-- Exercise 3: Write a query to display the numbers sum 10 and 15;
+SELECT 10 + 15;
+-- Answer: 25;
