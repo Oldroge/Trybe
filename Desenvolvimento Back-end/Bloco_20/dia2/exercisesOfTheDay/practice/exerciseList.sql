a6ad9c894dcbfdb1331dc4c138b6562462861f20
OldRoge <rogeriomrjr@icloud.com>
2021-08-21 16:50:12 -0300

    Done exercise 2: Write a query to show three numbers and three columns

--- Desenvolvimento Back-end/Bloco_20/dia2/exercisesOfTheDay/practice/exerciseList.sql
+++ Desenvolvimento Back-end/Bloco_20/dia2/exercisesOfTheDay/practice/exerciseList.sql
@@ -1,2 +1,5 @@
 -- Exercise 1: Write a query to display string "This is SQL Exercise, Practice and Solution";
 SELECT 'This is SQL Exercise, Practice and Solution';
+
+-- Exercise 2: Write a query to show three numbers and three columns;
+SELECT 1 AS First_number, 2 AS second_number, 3 AS third_number;
