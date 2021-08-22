b1e3619e68d9c1b83a96ebba16365168876917c4
OldRoge <rogeriomrjr@icloud.com>
2021-08-22 19:23:31 -0300

    Done lesson 2

--- Desenvolvimento Back-end/Bloco_20/dia3/studyingContent/LIKE/lessons/lessonsList.sql
+++ Desenvolvimento Back-end/Bloco_20/dia3/studyingContent/LIKE/lessons/lessonsList.sql
@@ -1,2 +1,5 @@
 -- Lesson 1: Display all film details that contains the ace word in the name;
 SELECT * FROM film WHERE title LIKE '%ace%';
+
+-- Lesson 2: Display all filme details whose descriptions finish with china;
+SELECT * FROM film WHERE description LIKE '%china';
