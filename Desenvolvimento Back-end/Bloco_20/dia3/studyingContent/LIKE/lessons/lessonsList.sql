66ede14f674544a6d5ece4b7a3e98ff10511cc70
OldRoge <rogeriomrjr@icloud.com>
2021-08-22 19:35:33 -0300

    Done lesson 4

--- Desenvolvimento Back-end/Bloco_20/dia3/studyingContent/LIKE/lessons/lessonsList.sql
+++ Desenvolvimento Back-end/Bloco_20/dia3/studyingContent/LIKE/lessons/lessonsList.sql
@@ -7,3 +7,6 @@ SELECT * FROM film WHERE description LIKE '%china';
 -- Lesson 3: Display all details from two movies whose descriptions have the girl word and the title ends with
 -- the lord word;
 SELECT * FROM film WHERE description LIKE '%girl%' AND title LIKE '%lord';
+
+-- Lesson 4: Display the two cases that from the 4º character in the film title, have the gon word;
+SELECT * FROM film WHERE title LIKE '___gon%';
