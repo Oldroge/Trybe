bb92f63d14ff278c38dbb4184ab932b40db3fb14
OldRoge <rogeriomrjr@icloud.com>
2021-08-22 14:44:43 -0300

    Creating exercises list - Done exercise 1: We need identify the customer datas with the e-mail: 'LEONARD.SCHOFIELD@sakilacustomer.org'

---
+++ Desenvolvimento Back-end/Bloco_20/dia3/studyingContent/lessonsList.sql
@@ -0,0 +1,3 @@
+-- Lesson 1: We need identify the customer datas with the e-mail: 'LEONARD.SCHOFIELD@sakilacustomer.org';
+USE sakila;
+SELECT * FROM customer WHERE email='LEONARD.SCHOFIELD@sakilacustomer.org';
