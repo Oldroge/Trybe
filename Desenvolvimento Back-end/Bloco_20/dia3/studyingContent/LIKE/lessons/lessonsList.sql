81fedf58384a5b23ec6a844f903b6b9f4eef2427
OldRoge <rogeriomrjr@icloud.com>
2021-08-22 19:28:46 -0300

    Done lesson 3

--- Desenvolvimento Back-end/Bloco_20/dia3/studyingContent/LIKE/lessons/lessonsList.sql
+++ Desenvolvimento Back-end/Bloco_20/dia3/studyingContent/LIKE/lessons/lessonsList.sql
@@ -3,3 +3,7 @@ SELECT * FROM film WHERE title LIKE '%ace%';
 
 -- Lesson 2: Display all filme details whose descriptions finish with china;
 SELECT * FROM film WHERE description LIKE '%china';
+
+-- Lesson 3: Display all details from two movies whose descriptions have the girl word and the title ends with
+-- the lord word;
+SELECT * FROM film WHERE description LIKE '%girl%' AND title LIKE '%lord';
