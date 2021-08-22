2f5c0d69de2b126f5578750e1a0b2f63dc1143a7
OldRoge <rogeriomrjr@icloud.com>
2021-08-22 17:27:59 -0300

    Done lesson 2: We need a client names report, in alphatic order, that dont are active in our system and belong the store with Id = 2, and dont includs the Kenneth customer in the result

--- Desenvolvimento Back-end/Bloco_20/dia3/studyingContent/lessonsList.sql
+++ Desenvolvimento Back-end/Bloco_20/dia3/studyingContent/lessonsList.sql
@@ -1,3 +1,8 @@
 -- Lesson 1: We need identify the customer datas with the e-mail: 'LEONARD.SCHOFIELD@sakilacustomer.org';
 USE sakila;
 SELECT * FROM customer WHERE email='LEONARD.SCHOFIELD@sakilacustomer.org';
+
+
+-- Lesson 2: We need a client names report, in alphatic order, that dont are active in our system and belong
+-- the store with Id = 2, and dont includs the Kenneth customer in the result;
+SELECT * FROM customer WHERE active = 0 AND store_id = 2 ORDER BY first_name <> 'Kenneth';
