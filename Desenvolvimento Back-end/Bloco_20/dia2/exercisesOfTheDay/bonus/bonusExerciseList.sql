60f0177e1b8dd13c158e0cf432f8785a3dae692b
OldRoge <rogeriomrjr@icloud.com>
2021-08-21 23:50:00 -0300

    Done exercise 2: Write a query to display all informations of the five pieces with the biggest price

--- Desenvolvimento Back-end/Bloco_20/dia2/exercisesOfTheDay/bonus/bonusExerciseList.sql
+++ Desenvolvimento Back-end/Bloco_20/dia2/exercisesOfTheDay/bonus/bonusExerciseList.sql
@@ -1,2 +1,5 @@
--- Exercise 1: Write a query to display the piece and the price of all that provided by employer RBT.
+-- Exercise 1: Write a query to display the piece and the price of all that provided by employer RBT;
 SELECT Piece, Price FROM Provides WHERE Provider='RBT';
+
+-- Exercise 2: Write a query to display all informations of the five pieces with the biggest price;
+SELECT * FROM Pieces, Providers, Provides ORDER BY Price DESC LIMIT 5;
