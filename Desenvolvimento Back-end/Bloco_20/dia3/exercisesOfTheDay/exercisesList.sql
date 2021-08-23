592c886ba65f38d818c61e6126f8237fa4b9be44
OldRoge <rogeriomrjr@icloud.com>
2021-08-23 12:18:38 -0300

    Done exercise 7

--- Desenvolvimento Back-end/Bloco_20/dia3/exercisesOfTheDay/exercisesList.sql
+++ Desenvolvimento Back-end/Bloco_20/dia3/exercisesOfTheDay/exercisesList.sql
@@ -17,8 +17,14 @@ SELECT * FROM Fornecedores
 WHERE name LIKE '%LTDA';
 
 -- Exercise 5: Write a query to display all emplyer numbers (providers) that contains the letter F in the code;
-SELECT COUNT(*) FROM Fornecedores WHERE code LIKE '%F%';
+SELECT COUNT(*) FROM Fornecedores
+WHERE code LIKE '%F%';
 
 -- Exercise 6: Write a query to display the supplies where the pieces cost more than R$15,00 and less than
 -- R$40,00. Order the results by ascending order;
-SELECT * FROM Fornecimentos WHERE Preco BETWEEN '15.00' AND '40.00';
+SELECT * FROM Fornecimentos
+WHERE Preco BETWEEN '15.00' AND '40.00';
+
+-- Exercise 7: Write a query to display the sales number done between the dates 2018-04-15 and 2019-07-30;
+SELECT COUNT(*) FROM Vendas
+WHERE DATE(order_date) BETWEEN '20018-04-15' AND '2019-07-30';
