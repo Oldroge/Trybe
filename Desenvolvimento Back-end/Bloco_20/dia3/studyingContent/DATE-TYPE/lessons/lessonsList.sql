3f6abac2aee3f8f9ef62e949a7a5d1df061ee4e6
OldRoge <rogeriomrjr@icloud.com>
2021-08-23 11:22:03 -0300

    Done Lesson 3

--- Desenvolvimento Back-end/Bloco_20/dia3/studyingContent/DATE-TYPE/lessons/lessonsList.sql
+++ Desenvolvimento Back-end/Bloco_20/dia3/studyingContent/DATE-TYPE/lessons/lessonsList.sql
@@ -8,3 +8,14 @@ WHERE DATE(payment_date) = '2005-05-25';
 SELECT COUNT(payment_date) FROM payment
 WHERE DATE(payment_date) BETWEEN
 '2005-07-01' AND '2005-08-22';
+
+-- Lesson 3: Using the rental table, extract date, year, month, day, hour, minute and second from records 
+-- with rental_id = 10330. Use the rental_date column to extract the informations;
+SELECT DATE(rental_date) AS Data,
+YEAR(rental_date) AS Ano,
+MONTH(rental_date) AS Mes,
+DAY(rental_date) AS Dia,
+HOUR(rental_date) AS Hora,
+MINUTE(rental_date) AS Minuto,
+SECOND(rental_date) AS Segundo
+FROM rental WHERE rental_id = 10330;
