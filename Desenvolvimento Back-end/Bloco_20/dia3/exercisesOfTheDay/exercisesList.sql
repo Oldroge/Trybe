c32eb8261d012f8c473f1516d6176f60a43405ab
OldRoge <rogeriomrjr@icloud.com>
2021-08-23 11:53:51 -0300

    Done exercise 3

--- Desenvolvimento Back-end/Bloco_20/dia3/exercisesOfTheDay/exercisesList.sql
+++ Desenvolvimento Back-end/Bloco_20/dia3/exercisesOfTheDay/exercisesList.sql
@@ -5,3 +5,8 @@ SELECT name FROM Pecas WHERE name like 'GR%';
 -- by provider alphatic order;
 SELECT * FROM Fornecimentos 
 WHERE code = 2 ORDER BY Fornecedor;
+
+-- Exercise 3: Write a query to display the pieces, price and providers for all supplies that the provider code
+-- have the letter N;
+SELECT peca, Preco, Fornecedor FROM Fornecimentos
+WHERE Fornecedor LIKE '%N%';
