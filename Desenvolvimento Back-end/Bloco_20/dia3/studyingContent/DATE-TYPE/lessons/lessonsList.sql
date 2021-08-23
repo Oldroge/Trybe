c5a9dcfef934b0ed05d39fc97ec0247bfcc75e3b
OldRoge <rogeriomrjr@icloud.com>
2021-08-23 11:09:32 -0300

    Done lesson 2

--- Desenvolvimento Back-end/Bloco_20/dia3/studyingContent/DATE-TYPE/lessons/lessonsList.sql
+++ Desenvolvimento Back-end/Bloco_20/dia3/studyingContent/DATE-TYPE/lessons/lessonsList.sql
@@ -1,4 +1,10 @@
 -- Lesson 1: How much payments we have with the return date 2005-05-25? There are multiples possible ways 
 -- to find this answer;
-SELECT count(payment_date) FROM sakila.payment
+USE sakila;
+SELECT count(payment_date) FROM payment
 WHERE DATE(payment_date) = '2005-05-25';
+
+-- Lesson 2: How much payments were done between 2005-07-01 and 2005-08-22?
+SELECT COUNT(payment_date) FROM payment
+WHERE DATE(payment_date) BETWEEN
+'2005-07-01' AND '2005-08-22';
