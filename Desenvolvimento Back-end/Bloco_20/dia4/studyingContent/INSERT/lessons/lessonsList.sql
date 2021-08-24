6410e5af8665af159443607399ef4bcbee1918b1
OldRoge <rogeriomrjr@icloud.com>
2021-08-24 00:15:33 -0300

    Done lesson 2

--- Desenvolvimento Back-end/Bloco_20/dia4/studyingContent/INSERT/lessons/lessonsList.sql
+++ Desenvolvimento Back-end/Bloco_20/dia4/studyingContent/INSERT/lessons/lessonsList.sql
@@ -2,7 +2,6 @@
 -- To know which field are required, click with the right button in the table sakila.staff and select
 -- "table inspector". Click in the tab "columns" and verify which fields accept nulls to guide you. 
 -- Remember that values are generated automatically and dont need be insert manually.
-SELECT * FROM staff;
 INSERT INTO staff (
 first_name,
 last_name,
@@ -22,3 +21,34 @@ password
 'Roge',
 'batman'
 );
+
+-- Lesson 2: Done the exercise before, lets now to level 2. Insert 2 new employees only one query;;
+INSERT INTO staff (
+first_name,
+last_name,
+address_id,
+email,
+store_id,
+active,
+username,
+password
+) VALUES (
+'Samuel',
+'Melo',
+3,
+'samuka@trybe.com',
+2,
+1,
+'Samuka',
+'oiaOtrem'
+), (
+'Frank',
+'Rocha',
+3,
+'frankito@trybe.com',
+1,
+1,
+'Frank',
+'curtoRock'
+);
+SELECT * FROM staff;
