2c290d478dd6b7ebb411524bda79ae451a42da6d
OldRoge <rogeriomrjr@icloud.com>
2021-09-02 10:53:01 -0300

    Add example about primary key and group primary key

--- Desenvolvimento Back-end/Bloco_22/dia1/studyingContent/createDesign/primaryKeyExample.sql
+++ Desenvolvimento Back-end/Bloco_22/dia1/studyingContent/createDesign/primaryKeyExample.sql
@@ -16,3 +16,18 @@
     VALUES('Rio Claro','SP',185421),
           ('Rio Claro','RJ',17216);
 	-- Happen an error cause the PK is only City and the PK cant repeat;
+    -- To fix this we can add Primary Key Group and whether one of the keys repeat the shouldn't, like in
+    -- the example below:
+    
+        DROP SCHEMA IF EXISTS Brasil;
+    CREATE SCHEMA Brasil;
+    USE Brasil;
+
+    CREATE TABLE cidades(
+        cidade VARCHAR(100) NOT NULL,
+        estado VARCHAR(10) NOT NULL,
+        populacao INTEGER,
+        CONSTRAINT PRIMARY KEY(cidade, estado)
+    );
+    
+    -- Now the primary keys is as cidade as estado and if any of the keys values repeat, the other to recover;
