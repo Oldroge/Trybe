1c16e13dd5ef2b81e8d3f98b4ad78c7c094bc718
OldRoge <rogeriomrjr@icloud.com>
2021-09-24 11:30:56 -0300

    Done exercises about how to make a draw lots

---
+++ Desenvolvimento Back-end/Bloco_26/Dia1/exercisesOfTheDay/sorteio.js
@@ -0,0 +1,18 @@
+const readline = require('readline-sync');
+
+const logResults = (number, answer) => {
+  if (answer === number) {
+    return console.log("Congrats! Right number!")
+  };
+  return console.log(`Oh! Try again, the number were: ${number}`)
+}
+
+const drawLots = () => {
+  const randomNum = (Math.ceil(Math.random() * 10));
+  console.log("Try to guess witch number I'm thinking...")
+
+  const number = readline.questionInt('Type a number between 0 and 10? ')
+
+  logResults(randomNum, number)
+}
+drawLots();
