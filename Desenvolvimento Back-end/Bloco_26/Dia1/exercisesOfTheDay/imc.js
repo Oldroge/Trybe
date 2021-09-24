1591a97fcf12fa7df74f86976887029ab17e4ef4
OldRoge <rogeriomrjr@icloud.com>
2021-09-24 10:06:11 -0300

    Add a imc table to classificate

--- Desenvolvimento Back-end/Bloco_26/Dia1/exercisesOfTheDay/imc.js
+++ Desenvolvimento Back-end/Bloco_26/Dia1/exercisesOfTheDay/imc.js
@@ -1,12 +1,31 @@
 const readline = require('readline-sync');
 
-
-
 const calculateImc = () => {
   const height = readline.questionInt('Which your height? ');
   const weight = readline.questionFloat('Which you weight? ');
   console.log(`Your height is ${ height } and your weight is ${ weight }`);
   const imc = (weight / Math.pow(height / 100, 2)).toFixed(2);
-  console.log(imc);
-}
+  
+    if (imc < 18.5) {
+      console.log("Situação: Abaixo do peso (magreza)");
+      return;
+    };
+    if (imc > 18.5 && imc < 24.9) {
+      console.log("Situação: Peso normal");
+      return;
+    };
+    if (imc > 25 && imc < 29.9) {
+      console.log("Situação: Acima do peso (sobrepeso)");
+      return;
+    }
+    if (imc > 30 && imc < 34.9) {
+      console.log("Situação: Obesidade grau I");
+      return;
+    }
+    if (imc > 35 && imc < 39.9) {
+      console.log("Situação: Obesidade grau II");
+      return;
+    };
+  console.log("Obesidade graus III e IV ");
+};
 calculateImc();
