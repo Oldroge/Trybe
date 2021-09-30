3a2bf051e64a6b44a7c439a3f98b52f80fe31743
OldRoge <rogeriomrjr@icloud.com>
2021-09-30 00:24:26 -0300

    Done exercise 2 - complement the last exercise

--- Desenvolvimento Back-end/Bloco_26/Dia2/exercisesOfTheDay/exercise2.js
+++ Desenvolvimento Back-end/Bloco_26/Dia2/exercisesOfTheDay/exercise2.js
@@ -4,12 +4,25 @@ const fs = require('fs/promises');
 
 const array = ['Finalmente', 'estou', 'usando', 'Promise.all', '!!!'] 
 
-function usePromiseAll() {
+async function usePromiseAll() {
   const addItem = (item, index) => {
     Promise.all([
       fs.writeFile(`file${index + 1}.txt`, item),
     ])
   };
   array.forEach(addItem);
+
+  const phrase = await Promise.all([
+    fs.readFile(`file1.txt`, 'utf-8'),
+    fs.readFile(`file2.txt`, 'utf-8'),
+    fs.readFile(`file3.txt`, 'utf-8'),
+    fs.readFile(`file4.txt`, 'utf-8'),
+    fs.readFile(`file5.txt`, 'utf-8')
+  ])
+  .then((results) => results
+  .join(' '));
+  fs.writeFile(`fileAll.txt`, JSON.stringify(phrase));
 };
 usePromiseAll();
+
+// Now programe your function for what it does the read of all files created before, store this infos and write in a file called fileAll.txt
