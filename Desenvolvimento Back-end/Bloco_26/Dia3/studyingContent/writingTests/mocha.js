e8aaff9b67ce3d95fb089617c28c5c8123b62fce
OldRoge <rogeriomrjr@icloud.com>
2021-10-02 13:43:49 -0300

    Add examples about how to use mocha

---
+++ Desenvolvimento Back-end/Bloco_26/Dia3/studyingContent/writingTests/mocha.js
@@ -0,0 +1,14 @@
+// Structuring tests with Mocha;
+
+describe('When the average were less than 7', () => {
+  //
+})
+
+//Describe accept 2 params: the first one is the description that put a resum about what the test tests, and second and the second execute a function to execute the test scenary. It's not necessary import the mocha, your reserved words will be interpreted when execute the test.
+
+// Now using the it syntax is similar the describe, and always is used inside a describe, the it describe what really will be tested:
+describe('When the average were less than 7', () => {
+  it('retorna "reprovado"', () => {
+    //
+  });
+})
