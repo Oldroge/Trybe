a6e1c1941c6f24b1e987ca4b01ca99902b565f0d
OldRoge <rogeriomrjr@icloud.com>
2021-10-03 20:15:04 -0300

    Try

---
+++ Desenvolvimento Back-end/Bloco_26/Dia3/studyingContent/writingTests/writingTests.js
@@ -0,0 +1,8 @@
+// The first step to write tests is to understand the sctructure that want to have and the expected behaviors.
+
+// About the structure:
+// The function receives a parameter called "average";
+// Answer as "disapproved" or "approved";
+
+// About the expected behaviors:
+// 1 - If the value is less than 7 should be "disapproved"; 2 - If passed a value bigger than 7, the answer expected should be "approved"; 3 - What if the value equals 7 the answer should be "approved";
