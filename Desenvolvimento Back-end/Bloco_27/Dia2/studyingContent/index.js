aa60800b47606defa0d6c4e563a515c86f8bff3c
OldRoge <rogeriomrjr@icloud.com>
2021-10-18 10:30:48 -0300

    Fixed some errors

--- Desenvolvimento Back-end/Bloco_27/Dia2/studyingContent/index.js
+++ Desenvolvimento Back-end/Bloco_27/Dia2/studyingContent/index.js
@@ -4,14 +4,17 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const Author = require('./controllers/author');
+const errorMiddleware = require('./middlewares/error');
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.get('/authors', Author.getAll());
-app.get('/authors/:id', Author.findById());
-app.post('/authors', Author.create());
+app.get('/authors', Author.getAll);
+app.get('/authors/:id', Author.findById);
+app.post('/authors', Author.create);
+
+app.use(errorMiddleware);
 
 const PORT = process.env.PORT || 3000;
 
