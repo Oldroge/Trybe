98f1541724bfa0093c4aac9bee352784ffd0077b
OldRoge <rogeriomrjr@icloud.com>
2021-11-24 22:44:17 -0300

    Add the same function used in the index.js but now using the SOLID principle

---
+++ Desenvolvimento Back-end/Bloco_30/Dia1/studyingContent/Single-responsability-principle/index2.js
@@ -0,0 +1,66 @@
+// ./index.js
+
+/* "Convert" */
+const percentageGradesIntoLetters = ({ name, disciplines }) => ({
+  name,
+  disciplines: disciplines.map(({ name, grade }) => {
+    let letterGrade;
+
+    if (grade >= 0.9) letterGrade = 'A';
+    else if (grade >= 0.8) letterGrade = 'B';
+    else if (grade >= 0.7) letterGrade = 'C';
+    else if (grade >= 0.6) letterGrade = 'D';
+    else if (grade >= 0.1) letterGrade = 'E';
+    else letterGrade = 'F';
+
+    return { name, grade, letterGrade };
+  })});
+
+/* "Define" */
+const approvedStudents = ({ disciplines }) =>
+  disciplines.every(({ grade }) => grade > 0.7);
+
+/* "Update" */
+const updateApprovalData = ({ name: studentName, disciplines }) => {
+  console.log(`A pessoa com nome ${studentName} foi aprovada!`);
+
+  disciplines.map(({ name, letterGrade }) =>
+    console.log(`${name}: ${letterGrade}`));
+};
+
+function setApproved(students) {
+  students
+    .map(percentageGradesIntoLetters)
+    .filter(approvedStudents)
+    .map(updateApprovalData);
+}
+
+/* execution example */
+const students = [
+  {
+    name: 'Lee',
+    disciplines: [
+      { name: 'matemática', grade: 0.8 },
+      { name: 'história', grade: 0.6 },
+    ],
+  },
+  {
+    name: 'Clementine',
+    disciplines: [
+      { name: 'matemática', grade: 0.8 },
+      { name: 'história', grade: 0.9 },
+    ],
+  },
+];
+
+setApproved(students);
+
+/*
+  Dont forget that is necessary export at the functions to test it all
+*/
+module.exports = {
+  percentageGradesIntoLetters,
+  approvedStudents,
+  updateApprovalData,
+  setApproved,
+};
