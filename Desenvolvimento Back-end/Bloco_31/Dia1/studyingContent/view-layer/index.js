66209386af10cc024b59b4c99cc57bec9c924a48
OldRoge <rogeriomrjr@icloud.com>
2022-01-05 22:20:25 -0300

    Exercises

--- Desenvolvimento Back-end/Bloco_31/Dia1/studyingContent/view-layer/index.js
+++ Desenvolvimento Back-end/Bloco_31/Dia1/studyingContent/view-layer/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const authorController = require('./controllers/authorController');
 
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set('view engine', 'ejs');
 
@@ -10,9 +13,13 @@ app.set('views', './views');
 
 app.get('/authors', authorController.listAuthors);
 
+app.get('/authors/new', authorController.newAuthor);
+
 app.get('/authors/:id', authorController.showAuthor);
 
-const PORT = process.env.PORT || 3000;
+app.post('/authors', authorController.createAuthor);
+
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
   console.log(`Ouvindo a porta ${PORT}`);
