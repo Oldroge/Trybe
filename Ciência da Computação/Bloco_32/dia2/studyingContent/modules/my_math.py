2e68ce119ae530d68d5c4bbbf267c4aa216bb03a
OldRoge <rogeriomrjr@icloud.com>
2022-01-26 20:50:21 -0300

    study: learning module concepts

---
+++ Ciência da Computação/Bloco_32/dia2/studyingContent/modules/my_math.py
@@ -0,0 +1,13 @@
+def sum(a, b):
+  return a + b
+
+
+def pot(a, b):
+  return a ** b
+
+
+print(sum(2, 2))
+print(pot(2, 3))
+
+# All files in python can be called as a modules.
+# Modules are files that have definitions and instructions in python, modules can be imported inside the other files and use your classes, functions etc.
