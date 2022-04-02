0c3e41303b848437f9017df568da3c2b4b20bb45
OldRoge <rogeriomrjr@icloud.com>
2022-04-01 21:59:28 -0300

    feat: set the name, email, CPF and address inputs

--- Desenvolvimento Front-end/Exercises/Bloco_12/review/my-form-2.0/src/App.js
+++ Desenvolvimento Front-end/Exercises/Bloco_12/review/my-form-2.0/src/App.js
@@ -1,9 +1,76 @@
 import React, { Component } from 'react'
 
+import './App.css'
+
 export default class App extends Component {
+  constructor() {
+    super();
+    this.handleState = this.handleState.bind(this)
+
+    this.state = {
+      address: ''
+    }
+  }
+
+  handleState({ target }) {
+    const { name, value } = target
+    console.log(this.state.address)
+    if (name === 'address') {
+      this.setState({
+        address: value.replace(/[!@#$%&*()=+|/]/g, "")
+      })
+    }
+    else {
+      this.setState({
+        [name]: value
+      })
+    }
+
+  }
+
   render() {
     return (
-      <div>App</div>
+      <fieldset>
+        <label>
+          Nome: 
+          <input
+            id="name"
+            type="text"
+            maxLength="40"
+            required
+          />
+        </label>
+
+        <label>
+          Email: 
+          <input
+            type="text"
+            maxLength="50"
+            required
+          />
+        </label>
+
+        <label>
+          CPF: 
+          <input
+            type="text"
+            maxLength="11"
+            required
+          />
+        </label>
+
+        <label>
+        Endereço: 
+          <input
+            type="text"
+            name="address"
+            maxLength="200"
+            onChange={(value) => this.handleState(value)}
+            value={this.state.address}
+            required
+          />
+        </label>
+      </fieldset>
     )
   }
 }
