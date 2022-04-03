22624905e94934df487e978a271f549b008710cd
OldRoge <rogeriomrjr@icloud.com>
2022-04-02 23:40:22 -0300

    feat: create a button that send datas

--- Desenvolvimento Front-end/Exercises/Bloco_12/review/my-form-2.0/src/App.js
+++ Desenvolvimento Front-end/Exercises/Bloco_12/review/my-form-2.0/src/App.js
@@ -37,6 +37,7 @@ export default class App extends Component {
     super();
     this.handleState = this.handleState.bind(this)
     this.cantStartWithNumber = this.cantStartWithNumber.bind(this)
+    this.sendDataButton = this.sendDataButton.bind(this)
 
     this.state = {
       name: '',
@@ -45,7 +46,11 @@ export default class App extends Component {
       address: '',
       city: '',
       state: '',
-      type: ''
+      type: '',
+      resume: '',
+      office: '',
+      'office-description': '',
+      button: false
     }
   }
 
@@ -72,102 +77,168 @@ export default class App extends Component {
     }
   }
 
+  sendDataButton() {
+    this.setState({
+      button: true
+    })
+  }
+
   render() {
+    console.log(this.state.button)
     return (
-      <fieldset>
-        <label>
-          Nome: 
-          <input
-            id="name"
-            type="text"
-            name="name"
-            maxLength="40"
-            onChange={(value) => this.handleState(value)}
-            required
-          />
-        </label>
+      <>
+        <fieldset>
+          <label>
+            Nome: 
+            <input
+              id="name"
+              type="text"
+              name="name"
+              maxLength="40"
+              onChange={(value) => this.handleState(value)}
+              required
+            />
+          </label>
 
-        <label>
-          Email: 
-          <input
-            type="text"
-            name="email"
-            maxLength="50"
-            onChange={(value) => this.handleState(value)}
-            required
-          />
-        </label>
+          <label>
+            Email: 
+            <input
+              type="text"
+              name="email"
+              maxLength="50"
+              onChange={(value) => this.handleState(value)}
+              required
+            />
+          </label>
 
-        <label>
-          CPF: 
-          <input
-            type="text"
-            name="cpf"
-            maxLength="11"
-            onChange={(value) => this.handleState(value)}
-            required
-          />
-        </label>
+          <label>
+            CPF: 
+            <input
+              type="text"
+              name="cpf"
+              maxLength="11"
+              onChange={(value) => this.handleState(value)}
+              required
+            />
+          </label>
 
-        <label>
-        Endereço: 
-          <input
-            type="text"
-            name="address"
-            maxLength="200"
-            onChange={(value) => this.handleState(value)}
-            value={this.state.address}
-            required
-          />
-        </label>
+          <label>
+          Endereço: 
+            <input
+              type="text"
+              name="address"
+              maxLength="200"
+              onChange={(value) => this.handleState(value)}
+              value={this.state.address}
+              required
+            />
+          </label>
 
-        <label>
-        Cidade: 
-          <input
-            type="text"
-            name="city"
-            maxLength="28"
-            onChange={(value) => this.handleState(value)}
-            onBlur={(a) => this.cantStartWithNumber(a)}
-            value={this.state.city}
-            required
-          />
-        </label>
+          <label>
+          Cidade: 
+            <input
+              type="text"
+              name="city"
+              maxLength="28"
+              onChange={(value) => this.handleState(value)}
+              onBlur={(a) => this.cantStartWithNumber(a)}
+              value={this.state.city}
+              required
+            />
+          </label>
 
-        <label>
-        Estado: 
-          <select
-            name="state"
-            onChange={(value) => this.handleState(value)}
-            required
-          >
-            {
-              states.map((value, index) => (
-                <option key={ index }>{ value.uf }</option>
-              ))
-            }
-          </select>
-        </label>
+          <label>
+          Estado: 
+            <select
+              name="state"
+              onChange={(value) => this.handleState(value)}
+              required
+            >
+              {
+                states.map((value, index) => (
+                  <option key={ index }>{ value.uf }</option>
+                ))
+              }
+            </select>
+          </label>
 
-        <label>
-          Tipo:
-          <input
-            type="radio"
-            name="type"
-            value="CASA"
-            onChange={(value) => this.handleState(value)}
-            required
-          /> CASA
+          <label>
+            Tipo:
+            <input
+              type="radio"
+              name="type"
+              value="CASA"
+              onChange={(value) => this.handleState(value)}
+              required
+            /> CASA
 
-          <input
-            type="radio"
-            name="type"
-            value="APARTAMENTO"
-            onChange={(value) => this.handleState(value)}
-            required
-          /> APARTAMENTO
-        </label>
-      </fieldset>
+            <input
+              type="radio"
+              name="type"
+              value="APARTAMENTO"
+              onChange={(value) => this.handleState(value)}
+              required
+            /> APARTAMENTO
+          </label>
+        </fieldset>
+
+        <fieldset>
+          <label>
+            Resumo do currículo:
+            <textarea
+              name="resume"
+              maxLength="1000"
+              onChange={(value) => this.handleState(value)}
+              required
+            />
+          </label>
+
+          <label>
+            Cargo:
+            <textarea
+              name="office"
+              maxLength="40"
+              onChange={(value) => this.handleState(value)}
+              onMouseEnter={() => { alert('Preencha com cuidado esta informação.') }}
+              required
+            />
+          </label>
+          
+          <label>
+            Descrição do Cargo:
+            <textarea
+              name="office-description"
+              maxLength="500"
+              onChange={(value) => this.handleState(value)}
+              required
+            />
+          </label>
+        </fieldset>
+        <button
+          type="submit"
+          onClick={this.sendDataButton}
+        >
+          Enviar
+        </button>
+        {
+          this.state.button
+          ? (
+          <div>
+            <div>{ this.state.name }</div>
+            <div>{ this.state.email }</div>
+            <div>{ this.state.cpf }</div>
+            <div>{ this.state.address }</div>
+            <div>{ this.state.city }</div>
+            <div>{ this.state.state }</div>
+            <div>{ this.state.type }</div>
+            <div>{ this.state.resume }</div>
+            <div>{ this.state.office }</div>
+            <div>{ this.state['office-description'] }</div>
+          </div>
+          )
+          : null
+        }
+      </>
     )
   }
 }
