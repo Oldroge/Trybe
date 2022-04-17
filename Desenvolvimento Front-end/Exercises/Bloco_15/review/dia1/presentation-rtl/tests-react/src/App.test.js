1f6facff21190d14e5006c880449a0e24a117305
OldRoge <rogeriomrjr@icloud.com>
2022-04-17 12:32:23 -0300

    study: add a test that verifies if exist a button with especifics values

--- Desenvolvimento Front-end/Exercises/Bloco_15/review/dia1/presentation-rtl/tests-react/src/App.test.js
+++ Desenvolvimento Front-end/Exercises/Bloco_15/review/dia1/presentation-rtl/tests-react/src/App.test.js
@@ -10,14 +10,16 @@ test('Verificando se existe o campo Email.', () => {
   expect(inputEmail).toHaveProperty('type', 'email');
 });
 
-// test('Verificando se existe um botão', () => {
-//   render(<App />);
-//   const btn = screen.getByRole('button');
-//   expect(btn).toBeInTheDocument();
-// });
-
 test('Verificando se existe dois botões', () => {
   render(<App />);
   const buttons = screen.getAllByRole('button');
   expect(buttons).toHaveLength(2);
+});
+
+test('Verificando se existe um botão de enviar', () => {
+  render(<App />);
+  const btnSend = screen.getByTestId('id-send');
+  expect(btnSend).toBeInTheDocument();
+  expect(btnSend).toHaveProperty('type', 'button');
+  expect(btnSend).toHaveValue('Enviar');
 });
