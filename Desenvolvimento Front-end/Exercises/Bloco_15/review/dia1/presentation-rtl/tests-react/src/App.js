9e112976f942a5a2c1c75951c2ff4f361b5da3c1
OldRoge <rogeriomrjr@icloud.com>
2022-04-17 13:14:24 -0300

    study: change functional component to class componet for test new tools

--- Desenvolvimento Front-end/Exercises/Bloco_15/review/dia1/presentation-rtl/tests-react/src/App.js
+++ Desenvolvimento Front-end/Exercises/Bloco_15/review/dia1/presentation-rtl/tests-react/src/App.js
@@ -1,18 +1,20 @@
 // App.js
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <label htmlFor="id-email">
-        Email
-        <input id="id-email" type="email" />
-      </label>
-      <input id="btn-send" type="button" data-testid="id-send" value="Enviar" />
-      <input id="btn-back" type="button" value="Voltar" />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <label htmlFor="id-email">
+          Email
+          <input id="id-email" type="email" />
+        </label>
+        <input id="btn-send" type="button" data-testid="id-send" value="Enviar" />
+        <input id="btn-back" type="button" value="Voltar" />
+      </div>
+    );
+  }
 }
 
 export default App;
