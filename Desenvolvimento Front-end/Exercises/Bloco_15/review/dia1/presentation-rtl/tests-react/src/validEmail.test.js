bfd9300419b523644883646a844334d5106564a0
OldRoge <rogeriomrjr@icloud.com>
2022-04-20 17:13:06 -0300

    study: test if component change the text to green when email is right

--- Desenvolvimento Front-end/Exercises/Bloco_15/review/dia1/presentation-rtl/tests-react/src/validEmail.test.js
+++ Desenvolvimento Front-end/Exercises/Bloco_15/review/dia1/presentation-rtl/tests-react/src/validEmail.test.js
@@ -15,3 +15,10 @@ test('Testando um componente, caso o email seja inválido.', () => {
   const isValid = screen.getByText('Email Inválido');
   expect(isValid).toBeInTheDocument();
 });
+
+test('Testando se o componente fica com o texto verde com o e-mail correto.', () => {
+  const EMAIL_USER = 'email@email.com';
+  render(<ValidEmail email={EMAIL_USER} />)
+  const isGreen = screen.getByTestId('id-valid-email')
+  expect(isGreen).toHaveAttribute('class', 'green-text')
+})
