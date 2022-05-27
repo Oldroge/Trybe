6f64a1c69d68c3260f00c40fa47df6a92f11c01f
OldRoge <rogeriomrjr@icloud.com>
2022-05-26 22:01:23 -0300

    exercise: Create a greeter function

---
+++ Desenvolvimento Back-end/TypeScript/dia1/exercises/dist/exercises.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.greeter = void 0;
+function greeter(name) {
+    return `Olá ${name}!`;
+}
+exports.greeter = greeter;
---
+++ Desenvolvimento Back-end/TypeScript/dia1/exercises/index.ts
@@ -0,0 +1,3 @@
+import * as exercises from './exercises';
+
+console.log(exercises.greeter('Roge'))
