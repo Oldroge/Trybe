3ce28482a8b8d97e2852a8f633d82f93a1e9a93d
OldRoge <rogeriomrjr@icloud.com>
2022-05-29 21:32:36 -0300

    study: execute cli npx sequelize model:generate and create a table in database

---
+++ Desenvolvimento Back-end/Bloco_30/review/Dia1/studyingContent/app-with-sequelize/models/user.js
@@ -0,0 +1,8 @@
+module.exports = (sequelize, DataTypes) => {
+    const User = sequelize.define("User", {
+      fullName: DataTypes.STRING,
+      email: DataTypes.STRING,
+      phone_num: DataTypes.STRING,
+    })
+  return User;
+};
